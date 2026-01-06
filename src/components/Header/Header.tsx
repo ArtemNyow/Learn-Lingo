accb3d25ad35986c57ea4a87ce4d9e1a857cf4ee
Artem <tem4ik20.1@gmail.com>
2026-01-06 15:52:04 +0200

    create header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,45 @@
 import { Link } from "react-router-dom";
+import css from "./Header.module.css";
+import { FiLogIn } from "react-icons/fi";
 interface IHeader {
   isAuthenticated: boolean;
 }
 
 export default function Header({ isAuthenticated }: IHeader) {
   return (
-    <header>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/teachers">Teachers</Link>
-        {isAuthenticated && <Link to="/favorites">Favorites</Link>}
+    <header className={css.header}>
+      <Link to="/" className={css.logo}>
+        <svg width={28} height={28}>
+          <use href="./ukraine.svg"></use>
+        </svg>
+        LearnLingo
+      </Link>
+      <nav className={css.nav}>
+        <Link to="/" className={css.item}>
+          Home
+        </Link>
+        <Link to="/teachers" className={css.item}>
+          Teachers
+        </Link>
+        {isAuthenticated && (
+          <Link to="/favorites" className={css.item}>
+            Favorites
+          </Link>
+        )}
       </nav>
+      <ul className={css.authList}>
+        <li className={css.authItem}>
+          <button type="button" className={css.logBtn}>
+            <FiLogIn className={css.svgLogo} />
+            Log in
+          </button>
+        </li>
+        <li className={css.authItem}>
+          <button className={css.regBtn} type="button">
+            Register
+          </button>
+        </li>
+      </ul>
     </header>
   );
 }
